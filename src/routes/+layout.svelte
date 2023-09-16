6c6a2072ec145bc376155c3fee7e119f18bd7398
sveltekits <sveltekit.com@gmail.com>
2023-09-16 10:38:23 +0900

    [create] app.css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<div class="main-warp">
+	<div class="main-content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.main-warp {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.main-content {
+		border: 1px solid rgb(222, 221, 221);
+		width: var(--body-max-width);
+	}
+</style>
